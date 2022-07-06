ab67a48d759ed40c164b6e08a788adedfa572a82
DmitryMalomoshin <makropulas@gmail.com>
2022-07-07 01:29:04 +0300

    Add DZ4Task25

---
+++ DZ4Task25/DZ4Task25.cs
@@ -0,0 +1,46 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+namespace DZ
+{
+    class Task25
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Введите число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num1;
+
+            Console.WriteLine("--------------------------------");
+
+            for (int i = 1; i < num2; i++)
+            {
+                result = result * num1;
+            }
+            Console.WriteLine($"Число {num1} возведённое в степень {num2} равняется {result}");
+        }
+    }
+}
+
+// Решение без цикла
+
+// namespace DZ
+// {
+//     class Task25
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.Clear();
+//             Console.Write("Введите число: ");
+//             int num1 = Convert.ToInt32(Console.ReadLine());
+//             Console.Write("Введите степень: ");
+//             int num2 = Convert.ToInt32(Console.ReadLine());
+
+//             Console.WriteLine("--------------------------------");
+
+//             Console.WriteLine($"Число {num1} в степени {num2} равняется {Math.Pow(num1, num2)}");
+//         }
+//     }
+// }
